25e6cee0382e510b46dab43e83faf47ea38a70ee
Jack Boynton <jboynton1999@gmail.com>
2019-07-04 10:10:06 -0400

    visuals

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import math
 import numpy
 from matplotlib import pyplot
 
-n = 500
+n = 50
 
 x_start, x_end = -5.0,5.0
 y_start, y_end = -10.0,10.0
@@ -19,3 +19,23 @@ pyplot.xlim(x_start,x_end)
 pyplot.ylim(y_start,y_end)
 pyplot.scatter(X,Y,s=5,color='#CD2305',marker='o')
 pyplot.show()
+
+## source
+
+strength = 5.0
+x_source, y_source = -1.0,0 ## location
+
+u_source = (strength / (2 * math.pi) * (X - x_source) / ((X - x_source)**2 + (Y - y_source)**2))
+v_source = (strength / (2 * math.pi) *(Y - y_source) / ((X - x_source)**2 + (Y - y_source)**2))
+
+## streams
+
+width = 10.0
+height = (y_end-y_start)/(x_end-x_start) * width
+pyplot.figure(figsize=(width,height))
+pyplot.xlim(x_start,x_end)
+pyplot.ylim(y_start,y_end)
+pyplot.streamplot(X,Y, u_source, v_source, density=2, linewidth=1, arrowsize=2, arrowstyle='->')
+
+pyplot.scatter(x_source,y_source,s=80,color='#CD2305',marker='o')
+pyplot.show() 
